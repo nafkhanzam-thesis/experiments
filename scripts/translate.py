5e21933fe7b8af87e6bb7d97da557a54bf51b5ff
Moch. Nafkhan Alzamzami <nafkhanalzamzami@gmail.com>
2022-12-29 14:44:34 +0700

    fix cuda

--- scripts/translate.py
+++ scripts/translate.py
@@ -2,13 +2,24 @@ import argparse
 from transformers import MarianMTModel, MarianTokenizer
 
 
+def assign_GPU(tokenizer_output):
+    tokenizer_output['input_ids'] = tokenizer_output['input_ids'].to('cuda')
+    # tokenizer_output['token_type_ids'] = tokenizer_output['token_type_ids'].to('cuda')
+    tokenizer_output['attention_mask'] = tokenizer_output['attention_mask'].to('cuda')
+
+    return tokenizer_output
+
+
 def create_translate_fn(model_name):
     tokenizer = MarianTokenizer.from_pretrained(model_name)
-    model = MarianMTModel.from_pretrained(model_name).to("cuda")
+    model = MarianMTModel.from_pretrained(model_name).to('cuda')
 
     def translate(en_text_list):
+        tokenizer_output = tokenizer(
+            en_text_list, return_tensors="pt", padding=True)
+        tokenizer_output = assign_GPU(tokenizer_output)
         translated = model.generate(max_new_tokens=528,
-                                    **tokenizer(en_text_list, return_tensors="pt", padding=True))
+                                    **tokenizer_output)
         return [tokenizer.decode(t, skip_special_tokens=True) for t in translated]
 
     return translate
