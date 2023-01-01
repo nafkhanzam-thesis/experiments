d41b7aa71fd2783ac38bfd04426c6543a9efc8dc
Moch. Nafkhan Alzamzami <nafkhanalzamzami@gmail.com>
2023-01-01 22:45:42 +0700

    add more bleu scripts

--- scripts/runners/en__en_back__bleu.py
+++ scripts/runners/en__en_back__bleu.py
@@ -17,20 +17,20 @@ if __name__ == "__main__":
         print(f"{data_source}-{split}-{idx} |", *msg)
 
     statement = db.session.prepare(
-        f"SELECT data_source, split, idx, en, en_alt, en__en_alt__bleu FROM {db.SCYLLA_DB_TABLE} WHERE data_source=? AND split=? AND idx>=? AND idx<?")
+        f"SELECT data_source, split, idx, en, en_back, en__en_back__bleu FROM {db.SCYLLA_DB_TABLE} WHERE data_source=? AND split=? AND idx>=? AND idx<?")
     statement.fetch_size = args.fetch_size
-    for data_source, split, idx, en, en_alt, en__en_alt__bleu in db.session.execute(statement, (args.data_source, args.split, args.range_l, args.range_r)):
-        if en__en_alt__bleu is not None:
+    for data_source, split, idx, en, en_back, en__en_back__bleu in db.session.execute(statement, (args.data_source, args.split, args.range_l, args.range_r)):
+        if en__en_back__bleu is not None:
             continue
         if en is None:
             log(idx, "en is null")
             continue
-        if en_alt is None:
-            log(idx, "en_alt is null")
+        if en_back is None:
+            log(idx, "en_back is null")
             continue
-        score = bleu_compute([en], [en_alt])
+        score = bleu_compute([en], [en_back])
         insert_statement = db.session.prepare(
-            f"UPDATE {db.SCYLLA_DB_TABLE} SET en__en_alt__bleu=? WHERE data_source=? AND split=? AND idx=?")
+            f"UPDATE {db.SCYLLA_DB_TABLE} SET en__en_back__bleu=? WHERE data_source=? AND split=? AND idx=?")
         db.session.execute(
             insert_statement, (score, args.data_source, args.split, idx))
         log(idx, "Inserted!", "BLEU score is", score)
