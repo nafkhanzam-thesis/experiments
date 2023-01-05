a43ffe6b1fd013b95f6461cdf314a0cbef7531ce
Moch. Nafkhan Alzamzami <nafkhanalzamzami@gmail.com>
2023-01-06 02:51:08 +0700

    change fp16

--- pre-train-amrbart-steps.sh
+++ pre-train-amrbart-steps.sh
@@ -1,20 +1,9 @@
+git clone https://github.com/NVIDIA/apex
+cd apex
+python -m pip install -v --disable-pip-version-check --no-cache-dir --global-option="--cpp_ext" --global-option="--cuda_ext" ./
+cd ..
 git clone https://github.com/nafkhanzam-thesis/experiments.git
 cd experiments
-change .gitmodules to:
-```
-[submodule "xl-amr-id"]
-        path = xl-amr-id
-        url = https://github.com/nafkhanzam-thesis/xl-amr-id.git
-[submodule "AMRBART"]
-        path = AMRBART
-        url = https://github.com/nafkhanzam-thesis/AMRBART.git
-[submodule "AMR-Process"]
-        path = AMR-Process
-        url = https://github.com/nafkhanzam-thesis/AMR-Process.git
-[submodule "transition-amr-parser"]
-        path = transition-amr-parser
-        url = https://github.com/nafkhanzam-thesis/transition-amr-parser.git
-```
 git submodule init
 git submodule sync
 git submodule update
@@ -25,15 +14,15 @@ mkdir datasets
 mkdir models
 sudo apt install unzip
 unzip amrbart.zip -d datasets
+curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
+sudo apt-get install git-lfs
+git lfs install
 pushd models
 git clone https://huggingface.co/facebook/mbart-large-50.git
 popd
 cd mbart-pre-train
-python -m pip install pathlib ruamel-yaml
-python -m pip install pillow-simd h5py-cache configargparse sacrebleu rouge-score datasets==2.4.0 transformers==4.21.3 nltk cached_property networkx penman>=1.1.0 pytorch-ignite regex smatch wandb amrlib PyYAML>=5.1
-python -m pip install pillow scipy tqdm scikit-learn gensim tensorboard tensorboardX
-pip install -v --no-cache-dir --global-option="--cpp_ext" --global-option="--cuda_ext" git+https://www.github.com/nvidia/apex
-nano run.sh
-copy paste run.sh content
-chmod +x run.sh
+python -m pip install --user pathlib ruamel-yaml
+python -m pip install --user pillow-simd h5py-cache configargparse sacrebleu rouge-score datasets==2.4.0 transformers==4.21.3 nltk cached_property networkx penman>=1.1.0 pytorch-ignite regex smatch wandb amrlib PyYAML>=5.1
+python -m pip install --user pillow scipy tqdm scikit-learn gensim tensorboard tensorboardX
+python -m pip install --user sentencepiece sentence-transformers
 bash run.sh
